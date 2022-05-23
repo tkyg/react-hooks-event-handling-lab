94dbd1c1d4d12ee715b3b0b12bd6a9bbc983a29b
tkyg <80114433+tkyg@users.noreply.github.com>
2022-05-23 18:22:35 -0400

    completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+  return (
+      <button 
+      onFocus={(e) => {console.log('Good!')}} 
+      onBlur={(e) =>{console.log('Hey! Eyes on me!')}}
+      >Eyes on me</button>
+  )
+}
+
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad(){
+  function change(event){
+    console.log(`${event.target.placeholder}`)
+  }
+
+  return (
+  <input 
+  type="password"
+  onChange={change}
+  placeholder="Entering password..."
+  />
+  )
+}
+
+export default Keypad;
